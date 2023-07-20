4e438323b865b18e54b9ebbe35dfbe0da1e5c177
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-07-20 22:47:58 +0200

    use explicit routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,4 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-Route::resource('books', BookController::class);
+
+// Define routes
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
+Route::get('/books/{isbn}', [BookController::class, 'show']);
+Route::put('/books/{isbn}', [BookController::class, 'update']);
+Route::delete('/books/{isbn}', [BookController::class, 'destroy']);
+
+// Alternative one-liner for generating all routes operations.
+// Route::resource('books', BookController::class);
+// Not sure if it works, since I use 'isbn' and not Laravels default 'id' as PK..
