ad568d6ac3315f1bf85ce463c2aa11f8b37f6e8e
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-07-25 13:22:34 -0400

    convert to more readable sql query

--- server/database/migrations/2023_07_20_123713_create_books_rated_view.php
+++ server/database/migrations/2023_07_20_123713_create_books_rated_view.php
@@ -12,7 +12,30 @@ return new class extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE OR REPLACE VIEW `books_rated` AS select `books`.`title` AS `title`,`books`.`author` AS `author`,`books`.`isbn` AS `isbn`,`books`.`publication_year` AS `publication_year`,`books`.`publisher` AS `publisher`,`books`.`image_url_s` AS `image_url_s`,`rt`.`avg_rating` AS `avg_rating`,`rt`.`num_ratings` AS `num_ratings` from (`books` left join (select `ratings`.`isbn` AS `isbn`,avg(`ratings`.`rating`) AS `avg_rating`,count(`ratings`.`user_id`) AS `num_ratings` from `ratings` group by `ratings`.`isbn`) `rt` on((`books`.`isbn` = `rt`.`isbn`)))");
+        DB::statement("
+            CREATE OR REPLACE VIEW `books_rated` AS
+            SELECT
+                `books`.`title` AS `title`,
+                `books`.`author` AS `author`,
+                `books`.`isbn` AS `isbn`,
+                `books`.`publication_year` AS `publication_year`,
+                `books`.`publisher` AS `publisher`,
+                `books`.`image_url_s` AS `image_url_s`,
+                `rt`.`avg_rating` AS `avg_rating`,
+                `rt`.`num_ratings` AS `num_ratings`
+            FROM
+                `books`
+            LEFT JOIN (
+                SELECT
+                    `ratings`.`isbn` AS `isbn`,
+                    AVG(`ratings`.`rating`) AS `avg_rating`,
+                    COUNT(`ratings`.`user_id`) AS `num_ratings`
+                FROM
+                    `ratings`
+                GROUP BY
+                    `ratings`.`isbn`
+            ) `rt` ON `books`.`isbn` = `rt`.`isbn`
+        ");
     }
 
     /**
